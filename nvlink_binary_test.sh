5d9c4e4ad6fc44db22ebf159b97c9c91a526d274
lifengli137 <54550920+lifengli137@users.noreply.github.com>
2022-08-17 13:46:27 -0700

    Update nvlink_binary_test.sh

--- nvlink_binary_test.sh
+++ nvlink_binary_test.sh
@@ -1,8 +1,7 @@
 echo "NVLINK tests through binary partitioning GPUs: "
-for i in 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 0,1,2,3,4,5,6,7 8,9,10,11,12,13,14,15 0,1,2,3 4,5,6,7 8,9,10,11 12,13,14,15 0,1 2,3 4,5 6,7 8,9 10,11 12,13 14,15 ; do
-        echo "CUDA_VISIBLE_DEVICES=$i"
-        n=`echo $i | tr -cd , | wc -c`;
-        ((n++))
-
-        CUDA_VISIBLE_DEVICES=$i mpirun --allow-run-as-root -np $n -mca pml ucx --mca btl ^vader,tcp,openib -x NCCL_SHM_DISABLE=1 -x NCCL_P2P_DISABLE=0 -x NCCL_IB_DISABLE=1  -x LD_LIBRARY_PATH=/opt/nccl-2.8.4-1/build/lib /opt/nccl-tests-2.11.0/build/alltoall_perf -b 2G -e 2G -c 1 -w 20 -n 50;
+for i in 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 0,1,2,3,4,5,6,7 8,9,10,11,12,13,14,15 0,1,2,3 4,5,6,7 8,9,10,11 12,13,14,15 0,1 2,3 4,5 6,7 8,9 10,11 12,13 14,15 ; do 
+	echo "CUDA_VISIBLE_DEVICES=$i"
+	n=`echo $i | tr -cd , | wc -c`;
+	((n++))
+	CUDA_VISIBLE_DEVICES=$i mpirun --allow-run-as-root -np $n -mca pml ucx --mca btl ^vader,tcp,openib -x LD_LIBRARY_PATH=/opt/nccl-2.8.4-1/build/lib /opt/nccl-tests-2.11.0/build/alltoall_perf -b 2G -e 2G -c 1 -w 20 -n 50; 
 done
